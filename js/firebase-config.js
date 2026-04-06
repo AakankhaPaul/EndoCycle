c961b3ee2cd332c19893d7870c704dfeabf3a04a
Aakankha <akpra7850@gmail.com>
2026-04-06 16:30:08 +0530

    key config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,14 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+import ENV from "../config.js";
+
 // TODO: Replace the following with your app's Firebase project configuration
+import ENV from "../config.js";
 const firebaseConfig = {
-  apiKey: "AIzaSyAYIMBpNnGhcy-wzjmUtmOsBo9hU7vfrsc",
-  authDomain: "endocycle-43b86.firebaseapp.com",
-  projectId: "endocycle-43b86",
-  storageBucket: "endocycle-43b86.firebasestorage.app",
-  messagingSenderId: "253560551444",
-  appId: "1:253560551444:web:2ea0d511969ed8c4188f01"
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  projectId: ENV.FIREBASE_PROJECT_ID,
+  storageBucket: ENV.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: ENV.FIREBASE_MESSAGING_ID,
+  appId: ENV.FIREBASE_APP_ID
 };
 
 // Initialize Firebase App
@@ -23,3 +26,4 @@ const storage = getStorage(app);
 
 // Export the initialized services for use throughout your application
 export { app, auth, db, storage };
+
